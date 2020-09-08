a59f0609c7a4b08f00f9deebde16be95b9900442
haha <QMBFXSL@gmail.com>
2020-09-09 11:49:43 +1200

    wow zinga

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    //probably needed
+    [SerializeField] KeyCode openAtlasKey;
+    [SerializeField] LayerMask mapRayLayerMask;
+
+    //placeholder
+    [SerializeField] Camera mapCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(openAtlasKey))
+        {
+            if (mapCam.gameObject.activeSelf)
+            {
+                mapCam.gameObject.SetActive(false);
+            }
+            else
+            {
+                mapCam.gameObject.SetActive(true);
+            }
+        }
+
+       Ray ray = mapCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+        if(Physics.Raycast(ray, out rayHit, Mathf.Infinity, mapRayLayerMask))
+            {
+                if (rayHit.collider.gameObject.tag == "city")
+                    {
+
+                    }
+            }
+            
+
+    }
+}
