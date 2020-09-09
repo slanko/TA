aca17989d2c4fc425dd08addef594f94596215e9
haha <QMBFXSL@gmail.com>
2020-09-10 11:55:57 +1200

    fazoo
    added travel system

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -1,46 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class playerScript : MonoBehaviour
 {
+    public enum playerState
+    {
+        TRAVELLING,
+        STOPPED
+    }
     //probably needed
     [SerializeField] KeyCode openAtlasKey;
     [SerializeField] LayerMask mapRayLayerMask;
-
+    [SerializeField] List<GameObject> destinationList;
+    [SerializeField] playerState currentState = playerState.STOPPED;
+    NavMeshAgent nav;
+    cityScript currentCity;
     //placeholder
     [SerializeField] Camera mapCam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(openAtlasKey))
+        //map opening stuff
+        if (Input.GetKeyDown(openAtlasKey) && mapCam.gameObject.activeSelf)
         {
-            if (mapCam.gameObject.activeSelf)
+            mapCam.gameObject.SetActive(false);
+        }
+
+        if(currentState == playerState.STOPPED)
+        {
+            if (Input.GetKeyDown(openAtlasKey))
             {
-                mapCam.gameObject.SetActive(false);
+                if (mapCam.gameObject.activeSelf)
+                {
+                    mapCam.gameObject.SetActive(false);
+                }
+                else
+                {
+                    mapCam.gameObject.SetActive(true);
+                }
             }
-            else
+
+            //route selection stuff
+            Ray ray = mapCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+
+            if (Input.GetMouseButtonDown(0))
             {
-                mapCam.gameObject.SetActive(true);
+                Debug.Log("fired ray");
+                Debug.DrawRay(ray.origin, ray.direction, Color.red);
+                if (Physics.Raycast(ray, out rayHit, Mathf.Infinity, mapRayLayerMask))
+                {
+                    if (rayHit.collider.gameObject.tag == "City")
+                    {
+                        currentCity = rayHit.collider.gameObject.GetComponentInParent<cityScript>();
+                        destinationList.Add(currentCity.gameObject);
+                    }
+                    else
+                    {
+                        currentCity = null;
+                    }
+                }
             }
         }
 
-       Ray ray = mapCam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit rayHit;
-        if(Physics.Raycast(ray, out rayHit, Mathf.Infinity, mapRayLayerMask))
-            {
-                if (rayHit.collider.gameObject.tag == "city")
-                    {
+        //am i in city check
+        var distanceToDestination = nav.remainingDistance;
+        if (distanceToDestination < 2f)
+        {
+            reachedDestination();
+        }
+    }
 
-                    }
+    public void startTravelling()
+    {
+        nav.SetDestination(destinationList[0].gameObject.transform.position);
+        currentState = playerState.TRAVELLING;
+
+    }
+
+    public void clearRoute()
+    {
+        destinationList.Clear();
+    }
+
+    void reachedDestination()
+    {
+        if (currentState == playerState.TRAVELLING)
+        {
+            Debug.Log("destination reached!!");
+            destinationList.Remove(destinationList[0]);
+            if (destinationList.Count != 0)
+            {
+                startTravelling();
             }
-            
+            else
+            {
+                currentState = playerState.STOPPED;
+            }
+        }
 
     }
 }
