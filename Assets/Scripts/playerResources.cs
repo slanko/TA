11fd985a336cf9b16e5ca4f834ad510ccbfea5c0
haha <QMBFXSL@gmail.com>
2020-09-21 11:51:35 +1200

    you fricking died

---
+++ Assets/Scripts/playerResources.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerResources : MonoBehaviour
+{
+    public float playerHealth;
+    [SerializeField] Slider healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0)
+        {
+            //game OVERRRRRRRRRRRRR
+            Debug.Log("you fricking died");
+        }
+    }
+}
