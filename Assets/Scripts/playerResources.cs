d94133929d35b51f025118c5760d858a46de51a2
haha <QMBFXSL@gmail.com>
2020-09-25 11:50:47 +1200

    ferfefefrfe

--- Assets/Scripts/playerResources.cs
+++ Assets/Scripts/playerResources.cs
@@ -9,12 +9,6 @@ public class playerResources : MonoBehaviour
     public float playerHealth;
     [SerializeField] Slider healthBar;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/randomEventScript.cs
+++ Assets/Scripts/randomEventScript.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using static UnityEngine.UI.Button;
 
 [System.Serializable]
 public struct eventStruct
 {
     public string entryName;
     public eventData eventMaster;
-    public UnityEvent eventEffects;
+    public effectStruct[] eventEffects;
+}
+[System.Serializable]
+public struct effectStruct
+{
+    public string effectName;
+    public UnityEvent effect;
 }
 
 public class randomEventScript : MonoBehaviour
@@ -23,6 +30,8 @@ public class randomEventScript : MonoBehaviour
 
     [SerializeField, Header("Timer Stuff")] int randomEventTimeMin;
     [SerializeField] int randomEventTimeMax;
+
+    [SerializeField] GameObject buttonPrefab, buttonParent;
 
     IEnumerator randomEvent(int seconds)
     {
@@ -45,11 +54,20 @@ public class randomEventScript : MonoBehaviour
         Debug.Log("random event happens uya");
         eventPopup.SetActive(true);
         Time.timeScale = 0;
-        currentEvent = eventList[Random.Range(0, eventList.Count)].eventMaster;
+        var ev = eventList [Random.Range(0, eventList.Count)];
+        currentEvent = ev.eventMaster;
         eventHeader.text = currentEvent.eventTitle;
         eventCharacter.sprite = currentEvent.eventBeatList[eventCounter].characterSprite;
         eventCounter = 0;
         eventText.text = currentEvent.eventBeatList[eventCounter].beatText;
         StartCoroutine(randomEvent(Random.Range(randomEventTimeMin, randomEventTimeMax)));
+
+        foreach (var effect in ev.eventEffects)
+        {
+            // instantiate a button
+            var button = Instantiate(buttonPrefab, buttonParent.transform).GetComponent<Button>();
+            button.GetComponent<Text>().text = effect.effectName;
+            button.onClick = (ButtonClickedEvent)effect.effect;
+        }
     }
 }
