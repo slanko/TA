5a2ddeea2d07c4b3039876de68065d2507f39f96
slink <QMBFXSL@gmail.com>
2020-10-09 02:30:17 +1300

    shut UP

---
+++ Assets/Scripts/tradeItemScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tradeItemScript : MonoBehaviour
+{
+
+    [SerializeField] Slider valueSlider;
+    [SerializeField] Text valueText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        valueText.text = valueSlider.value.ToString();
+    }
+}
