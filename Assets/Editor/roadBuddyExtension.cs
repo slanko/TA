0d927dec5920d7ea8aa21931e692e9bce6536512
slink <QMBFXSL@gmail.com>
2020-10-23 01:19:05 +1300

    i love you christmas island

---
+++ Assets/Editor/roadBuddyExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(roadBuddy))]
+public class roadBuddyExtension : Editor
+{
+    roadBuddy roadB;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("align road!!!!"))
+        {
+            roadB.alignLinesToTerrain();
+        }
+    }
+
+    void OnEnable()
+    {
+        roadB = (roadBuddy)target;
+    }
+}
